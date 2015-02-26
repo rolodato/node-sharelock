b77296fd2ca8397b0335106e948ab3d003ae0402
Rodrigo López Dato <rodrigo@auth0.com>
2015-02-26 16:43:16 -0300

    Implement /create endpoint

--- sharelock.js
+++ sharelock.js
@@ -17,3 +17,33 @@ Sharelock.init = function (url) {
 	return new Sharelock(url);
 };
 
+Sharelock.prototype.create = function (secret, acl, cb) {
+	var uri = this.uri;
+	var options = {
+		uri: uri + '/create',
+		method: 'POST',
+		json: {
+			"d": secret,
+			"a": parseAcl(acl)
+		}
+	};
+	request.post(options, function (err, response, body) {
+		if (!err && response.statusCode === 200) {
+			return cb(null, uri + body);
+		} else {
+			return cb(err, null);
+		}
+	});
+};
+
+function parseAcl(acl) {
+	if (acl.constructor === Array && acl.length > 0) {
+		return acl.join(',');
+	} else if (ld.isString(acl)) {
+		return acl;
+	} else {
+		throw 'Invalid recipient list (ACL) specified: ' + acl;
+	}
+}
+
+module.exports = Sharelock;
