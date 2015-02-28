fed2523b0d94332eea12dc016262c3d944059542
Rodrigo López Dato <rodrigo@auth0.com>
2015-02-28 14:57:48 -0300

    Convert tabs to spaces

--- sharelock-client.js
+++ sharelock-client.js
@@ -2,47 +2,47 @@ var request = require('request');
 var validUrl = require('valid-url');
 
 function Sharelock (uri) {
-	if (!uri) {
-		this.uri = 'https://sharelock.io';
-	} else if (validUrl.isUri(uri)) {
-		this.url = uri;
-	} else {
-		throw 'Invalid Sharelock URI specified: ' + uri;
-	}
+  if (!uri) {
+    this.uri = 'https://sharelock.io';
+  } else if (validUrl.isUri(uri)) {
+    this.url = uri;
+  } else {
+    throw 'Invalid Sharelock URI specified: ' + uri;
+  }
 }
 
 // Enables `var sharelock = require('sharelock').init(url);`
 Sharelock.init = function (url) {
-	return new Sharelock(url);
+  return new Sharelock(url);
 };
 
 Sharelock.prototype.create = function (secret, acl, cb) {
-	var uri = this.uri;
-	var options = {
-		uri: uri + '/create',
-		method: 'POST',
-		json: {
-			"d": secret,
-			"a": parseAcl(acl)
-		}
-	};
-	request.post(options, function (err, response, body) {
-		if (!err && response.statusCode === 200) {
-			return cb(null, uri + body);
-		} else {
-			return cb(err, null);
-		}
-	});
+  var uri = this.uri;
+  var options = {
+    uri: uri + '/create',
+    method: 'POST',
+    json: {
+      "d": secret,
+      "a": parseAcl(acl)
+    }
+  };
+  request.post(options, function (err, response, body) {
+    if (!err && response.statusCode === 200) {
+      return cb(null, uri + body);
+    } else {
+      return cb(err, null);
+    }
+  });
 };
 
 function parseAcl(acl) {
-	if (acl.constructor === Array && acl.length > 0) {
-		return acl.join(',');
-	} else if (typeof acl === 'string') {
-		return acl;
-	} else {
-		throw 'Invalid recipient list (ACL) specified: ' + acl;
-	}
+  if (acl.constructor === Array && acl.length > 0) {
+    return acl.join(',');
+  } else if (typeof acl === 'string') {
+    return acl;
+  } else {
+    throw 'Invalid recipient list (ACL) specified: ' + acl;
+  }
 }
 
 module.exports = Sharelock;
